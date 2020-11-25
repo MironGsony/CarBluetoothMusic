3fcdf46a26928c6d13fe466332f746292bf430b0
Miron <sony23747251@163.com>
2020-11-25 18:20:46 +0800

    fix: add hidden file description

--- app/src/main/java/com/miron/carbtmusic/MainActivity.java
+++ app/src/main/java/com/miron/carbtmusic/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //本来打算写一个demo，懒了，还是提供一个蓝牙管理类的接口，使用的时候调用一下
+        // ,为了保证隐藏文件编译通过，这里提供了一个编译好的 framework.jar
     }
 }
